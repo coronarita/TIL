0f78a79c168482f2b2700772fbaa8aa88e168d5d
coronarita <davidwing4u@gmail.com>
2022-05-06 13:24:25 +0900

    Example3
    15의 배수일 때 yeardream 추가

--- hello.py
+++ hello.py
@@ -6,7 +6,9 @@
 print("hello")
 
 for i in range(1,21) :
-	if(i % 3) == 0 :
+	if(i%3) ==0 and (i%5) ==0 :
+		print("yearderam")
+	elif(i % 3) == 0 :
 		print("year")
 	elif( i% 5) == 0 : 
 		print("dream")
