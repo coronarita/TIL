6cc5bc745795814bc070d469d6039b095b2a01ea
coronarita <davidwing4u@gmail.com>
2022-05-06 13:21:28 +0900

    example2
    5의 배수일 때 dream 출력

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
-# 과제 : 1부터 10까지 반복, 그 과정에서 3의 배수일 경우 year 출력
+# 과제 : 1부터 20까지 반복, 그 과정에서 3의 배수일 경우 year 출력
 # 나머지 모든 경우 숫자 출력
+# 추가 과제1 : 5의배수 dream 출력
+# 추가과제2 : 15의 배수 yeardream 출력
+
 print("hello")
 
-for i in range(1,11) :
+for i in range(1,21) :
 	if(i % 3) == 0 :
 		print("year")
+	elif( i% 5) == 0 : 
+		print("dream")
 	else :
 		print(i)
