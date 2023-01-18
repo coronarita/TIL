b175ad05e7de5adc16f1e2dbbc56cfe94f64092c
coronarita <davidwing4u@gmail.com>
2023-01-19 08:21:20 +0900

    add : Comparing the speed between built-in python func and repeat

---
+++ python/speed_test.py
@@ -0,0 +1,17 @@
+import numpy as np
+from time import perf_counter
+
+result_list = []
+company_list = ["Samsung", "LG", "Google", "Apple", "Alibaba"]
+company_list_sample = np.repeat(company_list, 10**7)
+
+start = perf_counter()
+for company in company_list_sample:
+  result_list.append(company.lower())
+print(perf_counter()-start)
+# 17.13 s
+
+start = perf_counter()
+result_list = map(str.lower, company_list_sample)
+print(perf_counter()-start)
+# 0.97 s
