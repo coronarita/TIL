c6f212b7e33adbebfcc26f3b8eb6a7abd59d7398
Junghoon Lee <coronarita1991@gmail.com>
2024-04-14 23:14:12 +0900

    modify : using path parameter

--- FastAPI_python/Start_FastAPI/todos/todo.py
+++ FastAPI_python/Start_FastAPI/todos/todo.py
@@ -1,11 +1,13 @@
+# api.py에서 추가해줘야 함
 from fastapi import APIRouter
+from model import Todo
 
 todo_router = APIRouter()
 
 todo_list = []
 
 @todo_router.post("/todo")
-async def add_todo(todo: dict) -> dict:
+async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
     return {
         "message" : "Todo added successfully."
@@ -17,4 +19,14 @@ async def retrieve_todos() -> dict:
         "todos": todo_list
     }
     
-# api.py에서 추가해줘야 함
+@todo_router.get("/todo/{todo_id}") # 경로 매개변수
+# todo_id에 해당하는 작업 추출하는 라우트
+async def get_single_todo(todo_id: int) -> dict:
+	for todo in todo_list:
+		if todo.id == todo_id:
+			return {
+				"todo": todo
+			}
+	return {
+		"message" : "Todo with supplied ID doesn't exist."
+	}
